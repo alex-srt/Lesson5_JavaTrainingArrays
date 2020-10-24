d4a052718732a5e9a0c7f6ab0551a6a0755bc945
alex <lexsirant@gmail.com>
2020-10-24 22:21:44 +0300

    5L-minor

--- src/Task_1.java
+++ src/Task_1.java
@@ -1,15 +1,11 @@
-/*Increase by 2*/
+/*The sequence of integers is given. Increase by 2 each its non-negative element.*/
 
 import java.util.Arrays;
 
 public class Task_1 {
     public static void main(String[] args) {
         int[] arr = {2, 5, -5, 0, -4444, 555, 7};
-/*        for (int i : arr) {
-            if ( i>=0) {
-                arr[i]=i+2;
-            }
-        }*/
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 0) {
                 arr[i] = arr[i] + 2;
--- src/Task_2.java
+++ src/Task_2.java
@@ -1,8 +1,11 @@
-/*The first is not greater for 2,5*/
+/*Array of real numbers is given. Find the first element in array which value does not exceed 2.5.*/
 
 public class Task_2 {
     public static void main(String[] args) {
-        double arr[] = {12, 22.33, 111.222, 44.33, 2.7,2.1};
+        double[] arr = {12, 22.33, 111.222, 44.33, 2.6,2.7};
+        System.out.println(arr[arr.length-1]);
+        System.out.println(arr.length);
+        System.out.println(arr[5]);
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] <= 2.5) {
